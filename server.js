afb7acaee12847481ebc82cb98817a0a74962686
Your Name <neerajakshikoppisetty@gmail.com>
2025-07-11 15:34:35 +0530

    Add root route

--- server.js
+++ server.js
@@ -68,6 +68,11 @@ app.use((err, req, res, next) => {
 });
 
 // Start server
+
+app.get('/', (req, res) => {
+  res.send('Budget Beacon Backend is running!');
+});
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`✅ Server running on http://localhost:${PORT}`);
