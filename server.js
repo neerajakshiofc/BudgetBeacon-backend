5adbff1c5d32c3f0dfab532190fb705fb3b170f6
Your Name <neerajakshikoppisetty@gmail.com>
2025-07-17 18:29:19 +0530

    Your message about what changed

--- server.js
+++ server.js
@@ -101,3 +101,10 @@ const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`✅ Server running on http://localhost:${PORT}`);
 });
+
+require('dotenv').config();
+
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
